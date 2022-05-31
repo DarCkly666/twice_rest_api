792d83ce98692deb1091a8fc6e8d2a05678c4e44
DarCkly666 <lessombremonsieur@--->
2022-05-31 12:22:40 -0400

    Enviroment variables modified

--- db/connection.js
+++ db/connection.js
@@ -5,10 +5,14 @@ dotenv.config();
 const database = process.env.DATABASE;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
+const host = process.env.DB_HOST;
+const port = process.env.DB_PORT;
+const dialect = process.env.DB_DIALECT;
 
 const connection = new Sequelize(database, username, password, {
-  host: "localhost",
-  dialect: "mysql",
+  host: host,
+  port: port,
+  dialect: dialect,
 });
 
 module.exports = connection;
